4ba1e0f4e320b386681577f00600a1d818be6972
Eshan <3shan.bd@gmail.com>
2022-11-25 00:06:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('book worm server is running')
+});
+
+
+
+// mongodb
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.00o20sl.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('books-worm').collection('categories');
+        
+        const BooksCollection = client.db('books-worm').collection('books');
+        const UsersCollection = client.db('books-worm').collection('users');
+        const soldProductCollection = client.db('books-worm').collection('soldProduct');
+
+
+        app.get("/category",async (req, res) => {
+            const result = await categoriesCollection.find({}).toArray();
+         res.send(result);
+        });
+
+        // get books by category
+
+        app.get("/category/:books", async (req, res) => {
+          const books = req.params.books;
+            const query = { genre: books }
+            const result = await BooksCollection.find(query).toArray();
+            res.send(result);
+        });
+        
+        // get book details by book id 
+           app.get("/books/:id", async (req, res) => {
+               const id = req.params.id;
+             const query = { _id:ObjectId(id) };
+               const result = await BooksCollection.findOne(query);
+             res.send(result);
+           });
+
+       // user stored in data base
+      
+      app.post('/users', async (req, res) => {
+        const data = req.body.data;
+        const result = await UsersCollection.insertOne(data);
+        res.send(result);
+      })
+      
+      //user get 
+       app.get("/users", async (req, res) => {
+         const email = req.query.email;
+         const query = {email}
+         const result = await UsersCollection.findOne(query);
+         res.send(result);
+       });
+      
+      // collect sold product 
+      app.post('/buy', async (req, res) => {
+        const data = req.body.buyProduct;
+        const query = { _id: ObjectId(data?.ProductId) }
+        const updateDoc = {
+          $set: {
+            sold:true
+          }
+        }
+        const product = await BooksCollection.updateOne(query,updateDoc,{upsert:true})
+  
+        const result = await soldProductCollection.insertOne(data);
+        res.send(result);
+
+      })
+      
+    } 
+    finally {
+        
+    }
+}
+run().catch(err => console.log(err));
+
+
+
+app.listen(port, () => {
+    console.log('server is running')
+})
+
