e64feedeeac1a6a631d35546a2ccbc207602ee73
Eshan <3shan.bd@gmail.com>
2022-11-26 00:44:20 +0600

    advertise add

--- index.js
+++ index.js
@@ -207,7 +207,19 @@ async function run() {
       });
 
       // advertise Products
+      app.put('/advertise/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            advertise: true
+          }
+        };
+        const result = await BooksCollection.updateOne(query, updateDoc, { upsert: true });
+        res.send(result);
+      });
 
+      
       
     } 
     finally {
